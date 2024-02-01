4039b8b8261d2a8e0c944251ef263c2d6cbb7e5e
Iara Assis <iaraassisnega@gmail.com>
2024-02-01 18:33:00 -0300

    fix:Correcao

--- Frontend/src/pages/Errorpage/index.jsx
+++ Frontend/src/pages/Errorpage/index.jsx
@@ -4,8 +4,11 @@ export const ErrorPage = () =>{
     return(
         <div className={styles.error}>
             <h1 className="label">Error:<strong> 404!</strong></h1>
-            <img src="https://gifs.eco.br/wp-content/uploads/2022/04/gifs-de-cachorros-engracados-31.gif" alt="erro" />
-            <p className="label">Nao foi possivel encontrar a pagina!</p>
+            <img 
+                src="https://gifs.eco.br/wp-content/uploads/2022/04/gifs-de-cachorros-engracados-31.gif" 
+                alt="erro" 
+            />
+            <p className="label">Unable to find the page!</p>
         </div>
     )
 };
