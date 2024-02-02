12b9a4ed2819ec60d1aac54e4ed9ae9cf09ffe81
Iara Assis <iaraassisnega@gmail.com>
2024-02-02 14:39:06 -0300

    fix:tentativa de deploy vercel

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -9,7 +9,7 @@ import { handleErrors } from './middlewares/handleError.middleware';
 const app: Application = express();
 
 const corsOptions = {
-    origin:  ["http://localhost:5173" , "https://frontend-h4k2zcp0y-iaraassis.vercel.app"],
+    origin:  ["http://localhost:5173" , "https://frontend-jsecile8r-iaraassis.vercel.app"],
     credentials: true,
 };
 app.use(cors(corsOptions));
