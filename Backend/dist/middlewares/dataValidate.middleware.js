032c78d7c412e876a6a07d9e0bbba99034f554c2
Iara Assis <iaraassisnega@gmail.com>
2024-02-02 12:33:40 -0300

    fix:text3 render

--- Backend/dist/middlewares/dataValidate.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateValidation = void 0;
-const dateValidation = (schema) => (req, res, next) => {
-    const validatedDate = schema.parse(req.body);
-    req.body = validatedDate;
-    return next();
-};
-exports.dateValidation = dateValidation;
--- Backend/dist/schemas/login.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clientLoginSchema = void 0;
-const client_schema_1 = require("./client.schema");
-exports.clientLoginSchema = client_schema_1.clientSchema.pick({
-    email: true,
-    password: true,
-});
